761bae0ef2ae076fb2a02f36ec872d2fb8e06445
Spencer Kline <spencer.kline@uservoice.com>
2020-10-19 14:51:28 -0400

    TWEAK: Comma dangle should not be applied to single line (mostly for JSX use cases)

--- index.js
+++ index.js
@@ -2,10 +2,10 @@ module.exports = {
   extends: ['react-app', 'plugin:tailwind/recommended'],
   rules: {
     'no-mixed-operators': 0,
-    'semi': [2, 'always'],
+    'semi': ['error', 'always'],
     'quotes': ['error', 'single'],
     'jsx-quotes': ['error', 'prefer-double'],
-    'comma-dangle': ['error', 'always-multiline'],
+    'comma-dangle': ['error', 'only-multiline'],
     'indent': ['error', 2, {'SwitchCase': 1}],
     'no-console': ['error', { allow: ['warn', 'error'] }],
   },
